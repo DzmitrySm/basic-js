dc824a3144ff821cf46f64e0b02578ad9480ca92
DzmitrySm <magicjoker@yandex.ru>
2022-10-16 15:21:43 +0300

    fix: change task ext repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -20,14 +20,25 @@ function repeater(str, options) {
   if (options.repeatTimes === 1 && !options.separator && !options.addition) {
     return str
   }
+  
   for (let i = 0; i < options.repeatTimes; i++) {
-  if(options.repeatTimes > 1 && !options.separator && !options.additionSeparator) {
+    if (options.repeatTimes > 1 && !options.separator && options.addition && !options.additionSeparator) {
+     newArr.push(str, (options.addition + '|').repeat(options.additionRepeatTimes - 1), options.addition, '+')
+    }
+    if (options.repeatTimes > 1 && !options.separator && options.addition && options.additionSeparator) {
+      newArr.push(str, (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1), options.addition, '+')
+     }
+     if (options.repeatTimes > 1 && options.separator && options.addition && !options.additionSeparator && options.additionRepeatTimes !== 1) {
+      newArr.push(str, (options.addition + '|').repeat(options.additionRepeatTimes - 1), options.addition, options.separator )
+     }
+
+  if(options.repeatTimes > 1 && !options.separator && !options.additionSeparator && !options.addition) {
   newArr.push(str, '+')
   }
-  if(options.repeatTimes > 1 && options.separator && !options.addition) {
+  if(options.repeatTimes > 1 && options.separator && !options.addition && typeof options.addition !== 'boolean' && options.addition !== null) {
     newArr.push(str, options.separator)
  }
- if(options.repeatTimes > 1 && options.separator && options.addition && options.additionRepeatTimes && !options.additionSeparator) {
+ if(options.repeatTimes > 1 && options.separator && options.addition && options.additionRepeatTimes === 1 && !options.additionSeparator) {
   newArr.push(str, options.addition, options.separator)
   }
   if (!options.repeatTimes && !options.additionRepeatTimes) {
@@ -36,6 +47,16 @@ function repeater(str, options) {
   if(options.repeatTimes > 1 && options.separator && options.additionSeparator && options.addition) {
     newArr.push(str, (options.addition+options.additionSeparator).repeat(options.additionRepeatTimes - 1), options.addition, options.separator)
  }
+ if(options.repeatTimes > 1 && options.separator && options.additionSeparator && typeof options.addition === 'boolean') {
+  newArr.push(str, (options.addition+options.additionSeparator).repeat(options.additionRepeatTimes - 1), options.addition, options.separator)
+}
+if(options.repeatTimes > 1 && options.separator && options.additionSeparator && options.addition === null && str === null) {
+  newArr.push(str, (options.addition+options.additionSeparator).repeat(options.additionRepeatTimes - 1), options.addition, options.separator)
+}
+  }
+  if (options.separator && options.additionSeparator && options.addition && !options.repeatTimes) {
+    newArr.push(str, options.addition)
+    return newArr.join('')
   }
   
   return newArr.slice(0, newArr.length - 1).join('')
